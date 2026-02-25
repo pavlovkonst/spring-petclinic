ef55fe934ec61ccb31787afe2753ba69837c24ca
konstantin.pavlov <konstantin.pavlov@jetbrains.com>
2026-02-25 22:25:48 +0200

    Added GHA CI/CD, broke tests again

--- src/test/java/org/springframework/samples/petclinic/system/I18nPropertiesSyncTest.java
+++ src/test/java/org/springframework/samples/petclinic/system/I18nPropertiesSyncTest.java
@@ -1,6 +1,5 @@
 package org.springframework.samples.petclinic.system;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -37,7 +36,6 @@ public class I18nPropertiesSyncTest {
 
 	private static final Pattern HAS_TH_TEXT_ATTRIBUTE = Pattern.compile("th:(u)?text\\s*=\\s*\"[^\"]+\"");
 
-	//@Disabled("Temporarily ignored in CI")
 	@Test
 	public void thisTestShouldFail() {
 		fail("Intentional failure to test CI pipeline");
