2eb01f378a453993bbc7d3d966c8b5e5bd26145b
konstantin.pavlov <konstantin.pavlov@jetbrains.com>
2026-03-19 18:00:36 +0200

    fix: wrong status code in test

--- src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
@@ -53,7 +53,7 @@ public class PetClinicIntegrationTests {
 	void ownerDetails() {
 		RestTemplate template = builder.rootUri("http://localhost:" + port).build();
 		ResponseEntity<String> result = template.exchange(RequestEntity.get("/owners/1").build(), String.class);
-		assertThat(result.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+		assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 	@Test
